274b420a0a770f2a109d5d486b3542ae8eda160d
Niapa <niapa7@yandex.ru>
2023-06-19 19:59:29 +0700

    s3dz 1-2  commit

--- Program.cs
+++ Program.cs
@@ -15,6 +15,73 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 14212 -> нет
 12821 -> да
 23432 -> да
+___________________________
+1 копипаст: int[] InputSomeNums()
+{
+    Console.Write("Введите количество чисел: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+        else
+            Console.Write(".");
+    }
+}
+
+int[] myArray = InputSomeNums();
+PrintArray(myArray);
+__________________
+
+2 void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+        else
+            Console.Write(".");
+    }
+}
+
+int num = int.Parse(Console.ReadLine());
+int temp = num;
+
+int count = 0;
+while (temp != 0)
+{
+    temp = temp / 10; // num /= 10;
+    count++;
+}
+
+int[] array = new int[count];
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    array[i] = num % 10;
+    num /= 10;
+}
+
+PrintArray(array);
+_______________________
+
+
+
+
 */
 
 int[] InputSomeNums()
@@ -22,18 +89,16 @@ int[] InputSomeNums()
     Console.Write("Введите пятизначное число: ");
     int size = int.Parse(Console.ReadLine()!);
     int[] array = new int[size];
-       }
+    return array;
+}
 
 void PrintArray(int[] array)
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1 && array[0] == array[4] && array[1] == array[3] )
-            Console.WriteLine("Это палиндром");
-        else
-            Console.Write("Это не палиндром");
-    }
+    if (array[0] == array[4] ^ array[1] == array[3])
+        Console.WriteLine("Это палиндром");
+    else
+        Console.WriteLine("Это не палиндром");
 }
 
-int[] myArray = InputSomeNums();
-PrintArray(myArray);
+int[] oneArray = InputSomeNums();
+PrintArray(oneArray);
